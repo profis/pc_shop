417c3efc43bde1ef05d580f64d76cd916b0dfc38
Viacheslav Soroka <alpha@balticdev.com>
2015-03-16 13:11:28 +0200

    Added pageId parameter to PC_plugin_pc_shop_categories_widget widget.

--- widgets/PC_plugin_pc_shop_categories_widget.php
+++ widgets/PC_plugin_pc_shop_categories_widget.php
@@ -1,14 +1,20 @@
 <?php 
 
 class PC_plugin_pc_shop_categories_widget extends PC_plugin_pc_shop_widget {
-	
+
+	/** @var string */
 	protected $_template_group = 'categories';
-	
+
+	/** @var int|null */
 	protected $_category_id;
-	
+
+	/** @var int|null */
+	protected $_page_id;
+
 	public function Init($config = array()) {
 		parent::Init($config);
 		$this->_category_id = isset($this->_config['category']['id']) ? $this->_config['category']['id'] : null;
+		$this->_page_id = isset($this->_config['pageId']) ? $this->_config['pageId'] : null;
 	}
 	
 	public function get_params() {
@@ -35,7 +41,7 @@ class PC_plugin_pc_shop_categories_widget extends PC_plugin_pc_shop_widget {
 		$data = array();
 
 		$params = $this->get_params();
-		$data['categories']  = $this->shop->categories->Get(null, $this->_category_id, null, $params);
+		$data['categories']  = $this->shop->categories->Get(null, $this->_category_id, $this->_page_id, $params);
 		
 		return $data;
 	}
